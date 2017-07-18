ecfe8557e0c6b909cfbdbbc208869ab076d32edf
Simon Hempenstall <simonjhemp@hotmail.com>
2017-07-18 16:48:13 +0100

    Add to Html function and tests

--- spec/note-list-model-spec.js
+++ spec/note-list-model-spec.js
@@ -1,18 +1,15 @@
+var note = new Note("Too sexy to code");
+var noteList = new NoteList();
+
+
 function testNoteList(){
-  var noteList = new NoteList();
-  console.log(noteList);
-  console.log(noteList.noteModels);
-  // assert.isTrue(noteList.noteModels.length === 0);
   assert.isTrue(Array.isArray(noteList.noteModels));
 };
 
 testNoteList();
 
 function testNoteInArray(){
-  var noteList = new NoteList();
-  var note = new Note("Too sexy to code");
   noteList.pushToNoteModels(note);
-  console.log(noteList.noteModels);
   assert.isTrue(noteList.noteModels.length === 1);
 };
 
--- spec/note-list-view-spec.js
+++ spec/note-list-view-spec.js
@@ -1,8 +1,26 @@
+
+var note = new Note("Note message");
+var noteList = new NoteList();
+var noteListView = new NoteListView(noteList);
+
 function testNoteListView() {
-  var noteList = new NoteList();
-  var noteListView = new NoteListView(noteList);
   assert.isTrue(noteListView.noteList === noteList)
 };
 
 
 testNoteListView();
+
+function testoHTML() {
+  noteList.pushToNoteModels(note);
+  assert.isTrue(noteListView.toHtml() === "<ul><li><div>Note message</div></li><li><div></ul>")
+}
+
+testoHTML();
+
+function testtoHtmlNoNoteModels() {
+  var noteList2 = new NoteList();
+  var noteListView2 = new NoteListView(noteList2);
+  assert.isTrue(noteListView2.toHtml() === '<ul></ul>')
+}
+
+testtoHtmlNoNoteModels();
