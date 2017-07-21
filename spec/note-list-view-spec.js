1bf73162a49448105b0a0c4e4f031ccb31b16136
Verity <vhonebon@gmail.com>
2017-07-21 01:35:34 +0100

    Some refactoring of noteListView and note

--- spec/note-list-view-spec.js
+++ spec/note-list-view-spec.js
@@ -7,15 +7,15 @@ function testNoteListView() {
 
 testNoteListView();
 
-function testoHTML() {
+function testConvertToHTML() {
   var note = new Note("Lorem ipsum dolor sit amet, consectetur adipiscing elit.");
   var noteList = new NoteList();
   var noteListView = new NoteListView(noteList);
   noteList.pushToNotes(note);
-  assert.isEquals(noteListView.toHtml(), "<ul><li><div id='0'>Lorem ipsum dolor si</div></li></ul>")
+  assert.isEquals(noteListView.convertToHtml(), "<ul><li><div id='0'>Lorem ipsum dolor si</div></li></ul>")
 }
 
-testoHTML();
+testConvertToHTML();
 
 function testoMultipleHTML() {
   var note = new Note("Note message");
@@ -24,7 +24,7 @@ function testoMultipleHTML() {
   noteList.pushToNotes(note);
   noteList.pushToNotes(note2);
   var noteListView = new NoteListView(noteList);
-  assert.isTrue(noteListView.toHtml() === "<ul><li><div id='0'>Note message</div></li><li><div id='1'>Please fucking work</div></li></ul>")
+  assert.isTrue(noteListView.convertToHtml() === "<ul><li><div id='0'>Note message</div></li><li><div id='1'>Please fucking work</div></li></ul>")
 }
 
 testoMultipleHTML();
@@ -32,7 +32,7 @@ testoMultipleHTML();
 function testtoHtmlNoNoteModels() {
   var noteList = new NoteList();
   var noteListView = new NoteListView(noteList);
-  assert.isTrue(noteListView.toHtml() == null)
+  assert.isTrue(noteListView.convertToHtml() == null)
 }
 
 testtoHtmlNoNoteModels();
--- spec/note-model-spec.js
+++ spec/note-model-spec.js
@@ -7,8 +7,24 @@ function testNoteAttributes() {
 testNoteAttributes();
 
 function testGetText() {
-  var note = new Note("Javascript is sexy!");
-  assert.isTrue(note.getText() === "Javascript is sexy!");
+  var note = new Note("Javascript is the best!");
+  assert.isTrue(note.getText() === "Javascript is the best!");
 };
 
 testGetText();
+
+function testGetId() {
+  var note = new Note("Javascript is awesome!");
+  var notelist = new NoteList()
+  notelist.pushToNotes(note)
+  assert.isTrue(notelist.notes[0].id === 0);
+};
+
+testGetId();
+
+function testGetAbbreviatedText() {
+  var note = new Note("Javascript is so fun!");
+  assert.isTrue(note.getAbbreviatedText() === "Javascript is so fun");
+};
+
+testGetAbbreviatedText();
