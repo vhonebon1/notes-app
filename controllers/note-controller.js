24dd7b0a5df7f64bc7128edd4e97270f07ee837e
Verity <vhonebon@gmail.com>
2017-07-21 16:19:39 +0100

    Added event listener function

--- controllers/note-controller.js
+++ controllers/note-controller.js
@@ -3,12 +3,11 @@
     this.noteList = notelist;
     this.noteListView = new NoteListView(this.noteList)
   };
-  
+
   NoteController.prototype.addNoteToPage = function(element) {
     element.innerHTML = this.noteListView.convertToHtml();
   };
-  
-  
+
   NoteController.prototype.showNote = function(id, element) {
     var singleView = new SingleView(this.noteList.notes[id]);
     element.innerHTML = singleView.getText();
--- index.js
+++ index.js
@@ -3,22 +3,30 @@ window.onload = function() {
   var notelist = new NoteList()
   notelist.pushToNotes(note);
   var controller = new NoteController(notelist)
-  
+
   var element = document.getElementById("app")
   controller.addNoteToPage(element)
-  
+
   makeURLChangeShowIndividualNote()
-  
+
   function makeURLChangeShowIndividualNote() {
     window.addEventListener("hashchange", showIndividualNote);
   }
-  
+
   function showIndividualNote() {
     controller.showNote(getNoteIdFromUrl(window.location), element);
   }
-  
+
   function getNoteIdFromUrl(location) {
     return location.hash.split("#")[1].split("/")[1]
   }
-  
+
+  function submitListen() {
+    document.getElementById("text").addEventListener("submit", function(event) {
+      console.log(event);
+      event.preventDefault();
+    })
+  };
+
+submitListen();
 }
